aeb433d88d1168bbee248163f2a2ec34a7765b8d
Linh Tran <linhtanl51@gmail.com>
2025-08-06 21:32:23 -0400

    create dim_customers table

---
+++ dbt/olist_ecommerce/models/marts/core/dim_customers.sql
@@ -0,0 +1,20 @@
+with customers as (
+  select * from {{ ref('stg_customers') }}
+),
+
+geolocation as (
+    select 
+        geolocation_zip_code_prefix,
+        avg(geolocation_lat) as geolocation_lat,
+        avg(geolocation_lng) as geolocation_lng
+    from {{ ref('stg_geolocation') }}
+    group by geolocation_zip_code_prefix
+)
+
+select c.*,
+       g.geolocation_lat,
+       g.geolocation_lng
+
+from customers c
+left join geolocation g
+on c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
--- dbt/olist_ecommerce/models/staging/stg_geolocation.sql
+++ dbt/olist_ecommerce/models/staging/stg_geolocation.sql
@@ -6,5 +6,7 @@ with source_data as (
 
 select geolocation_zip_code_prefix,
        geolocation_city,
-       geolocation_state
+       geolocation_state,
+       cast(geolocation_lat as float) as geolocation_lat,
+       cast(geolocation_lng as float) as geolocation_lng
 from source_data
